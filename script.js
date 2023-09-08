d1abac4b7c6fc38d4a7b56aac955e3752e0db85f
thorcamara <thorcamaraalthoff@gmail.com>
2023-09-07 22:15:04 -0300

    Added EventListeners and function

--- script.js
+++ script.js
@@ -14,3 +14,45 @@ function voices() {
     voiceList.insertAdjacentHTML("beforeend", option);
   }
 }
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text) {
+    let utterance = new SpeechSynthesisUtterance(text);
+    for (let voice of synth.getVoices()) {
+        if (voice.name === voiceList.value) {
+            utterance.voice = voice;
+        }
+    }
+    synth.speak(utterance);
+}
+
+speechBtn.addEventListener("click", e => {
+    e.preventDefault();
+    if (textarea.value !== "") {
+        // Checks if not speaking, Speak Textarea Text
+        if (!synth.speaking) {
+            textToSpeech(textarea.value);
+        }
+        // If text was long, Add Resume and Pause Function
+        if (textarea.value.length > 80) {
+            setInterval(() => {
+                if (!synth.speaking && !isSpeaking) {
+                    isSpeaking = true;
+                    speechBtn.innerText = "Convert To Speech";
+                } else { }
+            }, 500);
+            if (isSpeaking) {
+                synth.resume();
+                isSpeaking = false;
+                speechBtn.innerText = "Pause Speech";
+            } else {
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerText = "Resume Speech";
+            }
+        } else {
+            speechBtn.innerText = "Convert To Speech";
+        }
+    }
+});
